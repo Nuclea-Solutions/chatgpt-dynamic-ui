5d036be5844f141c5e8e27952a434c7ec268e3b7
mareadelmar <82394561+mareadelmar@users.noreply.github.com>
2023-11-22 20:23:20 -0300

    Update: input view (#28)

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -3,40 +3,17 @@
 import InputWidthButtonComponent from '@/stories/input_with_button/InputWidthButton.component';
 // hooks and store
 import useChatCustom from '@/hooks/useChatCustom/useChatCustom';
-import useMessagesStore from '@/store/useMessagesStore';
-// utils
-import { COMPONENTS_REF } from '@/hooks/useChatCustom/utils';
-import HelpButtonComponent from '@/stories/help_button/HelpButton.component';
 
 const InputContainer = () => {
-	const { input, handleInputChange, handleSubmit, inputType } = useChatCustom();
-	const [messages, messagesComponents] = useMessagesStore((state) => [
-		state.messages,
-		state.messagesComponents
-	]);
-
-	const Component = !!messagesComponents.length
-		? messagesComponents?.at(-1)?.output
-			? COMPONENTS_REF[messagesComponents?.at(-1)?.output?.input_component ?? 'text_input']
-					.component
-			: !messagesComponents?.at(-1)?.output &&
-			  messagesComponents?.at(-1)?.content?.output?.input_component
-			? COMPONENTS_REF[messagesComponents?.at(-1)?.content?.output?.input_component ?? 'text_input']
-					.component
-			: COMPONENTS_REF['text_input'].component
-		: COMPONENTS_REF['text_input'].component;
+	const { input, handleInputChange, handleSubmit } = useChatCustom();
 
 	return (
 		<form
 			onSubmit={handleSubmit}
 			className='w-full lg:mx-auto lg:max-w-2xl xl:max-w-3xl bg-white dark:bg-[#444654] rounded-large relative'
 		>
-			{!!messagesComponents?.length && Component && (
-				<Component onChange={handleInputChange} value={input} />
-			)}
-			{messages?.every((item) => item.role === 'system') && inputType === 'text_input' && (
-				<InputWidthButtonComponent value={input} onChange={handleInputChange} />
-			)}
+			<InputWidthButtonComponent value={input} onChange={handleInputChange} />
+
 			{/* Footer */}
 			<div className='mt-2 text-center text-sm'>
 				{/* <span>ChatGPT can make mistakes. Consider checking important information.</span> */}
