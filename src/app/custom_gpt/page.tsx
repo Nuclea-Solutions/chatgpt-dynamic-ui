86702dc5fa66e169fcd440c7b71618915ee67f1d
mareadelmar <mar.ortega.v@gmail.com>
2023-12-06 17:41:14 -0300

    Update: custom GPT request

--- src/app/custom_gpt/page.tsx
+++ src/app/custom_gpt/page.tsx
@@ -1,10 +1,17 @@
 'use client';
+// libraries
 import React, { useState, useEffect } from 'react';
-import ToggleCustomGPT from '@/stories/second_phase/toggle_customGPT/ToggleCustomGPT.component';
-import InputWidthButtonComponent from '@/stories/input_with_button/InputWidthButton.component';
-import Avatar from '@/components/Avatar';
 import { HiOutlineCube } from 'react-icons/hi';
+// components
+import InputWidthButtonComponent from '@/stories/input_with_button/InputWidthButton.component';
+import ToggleCustomGPT from '@/stories/second_phase/toggle_customGPT/ToggleCustomGPT.component';
+import Avatar from '@/components/Avatar';
 import UploadImageComponent from '@/stories/second_phase/upload_image/UploadImage.component';
+import MessagesList from '@/components/MessagesList';
+// store and context
+import useCustomGPT from '@/store/useCustomGPT';
+import useMessagesStore from '@/store/useMessagesStore';
+import useChatCustom from '@/hooks/useChatCustom/useChatCustom';
 
 interface CheckboxState {
 	webBrowsing: boolean;
@@ -14,6 +21,19 @@ interface CheckboxState {
 
 const page = () => {
 	const [isActive, setIsActive] = useState('create');
+	const [name, description, instructions, setName, setDescription, setInstructions] = useCustomGPT(
+		(state) => [
+			state.name,
+			state.description,
+			state.instructions,
+			state.setName,
+			state.setDescription,
+			state.setInstructions
+		]
+	);
+	const { handleSubmit, input, handleInputChange } = useChatCustom({ customGPT: true });
+	const messages = useMessagesStore((state) => state.messages);
+
 	const handleActiveView = (activeView: string) => {
 		setIsActive(activeView);
 	};
@@ -84,8 +104,11 @@ const page = () => {
 							</div>
 						</div>
 
-						<form className='bg-white dark:bg-[#444654] rounded-large relative'>
-							<InputWidthButtonComponent value={''} onChange={() => {}} />
+						<form
+							className='bg-white dark:bg-[#444654] rounded-large relative'
+							onSubmit={handleSubmit}
+						>
+							<InputWidthButtonComponent value={input} onChange={handleInputChange} />
 						</form>
 					</div>
 
@@ -105,6 +128,8 @@ const page = () => {
 									id=''
 									placeholder='Name your GPT'
 									className='p-2 border rounded-[8px]'
+									value={name}
+									onChange={(e) => setName(e.target.value)}
 								/>
 							</div>
 
@@ -116,6 +141,8 @@ const page = () => {
 									id=''
 									placeholder='Add a shor description about what this GPT does'
 									className='p-2 border rounded-[8px]'
+									value={description}
+									onChange={(e) => setDescription(e.target.value)}
 								/>
 							</div>
 
@@ -127,6 +154,8 @@ const page = () => {
 									cols={50} // Número de columnas que se muestran
 									style={{ resize: 'vertical' }} // Permite redimensionar verticalmente
 									className='border rounded-[8px] outline-none p-2'
+									value={instructions}
+									onChange={(e) => setInstructions(e.target.value)}
 								/>
 							</div>
 
@@ -202,11 +231,21 @@ const page = () => {
 				>
 					<div className='flex flex-col justify-between h-full'>
 						<h4 className='flex justify-center '>Preview</h4>
-						<div className='flex justify-center '>
-							<HiOutlineCube size={50} />
-						</div>
-						<form className='w-full lg:mx-auto lg:max-w-2xl xl:max-w-3xl bg-white dark:bg-[#444654] rounded-large relative'>
-							<InputWidthButtonComponent value={''} onChange={() => {}} />
+
+						{!messages?.length ? (
+							<div className='flex justify-center '>
+								<HiOutlineCube size={50} />
+							</div>
+						) : (
+							<div>
+								<MessagesList messages={messages} />
+							</div>
+						)}
+						<form
+							className='w-full lg:mx-auto lg:max-w-2xl xl:max-w-3xl bg-white dark:bg-[#444654] rounded-large relative'
+							onSubmit={handleSubmit}
+						>
+							<InputWidthButtonComponent value={input} onChange={handleInputChange} />
 						</form>
 					</div>
 				</div>
---
+++ src/config/assistant-config.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_NAME = 'Chat Dynamic UI Assitant';
+export const DEFAULT_DESCRIPTION = 'Chat Dynamic UI Assitant';
+export const DEFAULT_MODEL = 'gpt-4-1106-preview';
+export const DEFAULT_INSTRUCTIONS = `
+
+`;
