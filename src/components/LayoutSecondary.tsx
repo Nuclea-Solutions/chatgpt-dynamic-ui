e31fa3062ac9543015f0fd2db4a8f05c521a9f3f
AlanAlexisNuclea <144055179+AlanAlexisNuclea@users.noreply.github.com>
2023-12-18 08:08:14 -0600

    Feat/gpst view components (#58)
    * update: 11/06/2023
    
    * update: updates saved
    
    * gpst-view, navbar, shareLintToChatModal

--- src/components/LayoutSecondary.tsx
+++ src/components/LayoutSecondary.tsx
@@ -5,12 +5,14 @@ import FeedbackModalComponent from '@/stories/feedback_modal/FeedbackModal.compo
 import { useFeedbackModal } from '../store/useFeedbackModal';
 import { usesidebar } from '@/store/useSidebar';
 import LayoutNavbar from './LayoutNavbar';
+import ShareLinkToChatComponent from '@/stories/share_link_to_chat/ShareLinkToChat.component';
+import { useshareLinkToChat } from '@/store/useLinkToChatComponent';
 
 const LayoutSecondary = ({ children }: { children: React.ReactNode }) => {
 	const openSidebar = usesidebar((state) => state.openSidebar);
 	const handleOpenSidebar = usesidebar((state) => state.setOpenSidebar);
 	const handleToggleSidebar = usesidebar((state) => state.setHandleToggleSidebar);
-
+	const openShareLinkToChat = useshareLinkToChat((state) => state.openShareLinkToChat);
 	const openModal = useFeedbackModal((state) => state.openModal);
 	const like = useFeedbackModal((state) => state.like);
 
@@ -33,6 +35,13 @@ const LayoutSecondary = ({ children }: { children: React.ReactNode }) => {
 				} `}
 			>
 				<FeedbackModalComponent like={like} />
+			</div>
+			<div
+				className={`fixed w-screen h-screen top-0 left-0 flex items-center z-40 bg-gray-700 bg-opacity-50 ${
+					!openShareLinkToChat && 'hidden'
+				}`}
+			>
+				<ShareLinkToChatComponent />
 			</div>
 		</>
 	);
