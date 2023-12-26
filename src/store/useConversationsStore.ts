bde02d608928dfd96f9da913d872b7ad72752674
mareadelmar <mar.ortega.v@gmail.com>
2023-12-26 19:25:09 -0300

    Fix: current conversation

--- src/store/useConversationsStore.ts
+++ src/store/useConversationsStore.ts
@@ -7,27 +7,42 @@ import { nanoid } from 'nanoid';
 interface ConversationsStore {
 	conversationList: Conversation[];
 	setConversationList: (conversations: Conversation[]) => void;
-	setNewMessageToConversation: (message: Message, conversationId: string) => void;
+	setNewMessageToConversation: (message: Message, conversationId: string | null) => void;
+	currentConversationId: string | null;
+	setCurrentConversationId: (conversationId: string | null) => void;
 }
 
 const useConversationsStore = create<ConversationsStore>((set) => ({
 	conversationList: [],
+	currentConversationId: null,
 	setConversationList: (conversations: Conversation[]) =>
 		set((state) =>
 			produce(state, (draft) => {
 				draft.conversationList = conversations;
 			})
 		),
-	setNewMessageToConversation: (message: Message, conversationId: string) =>
+	setNewMessageToConversation: (message: Message, conversationId: string | null) =>
 		set((state) =>
 			produce(state, (draft) => {
-				const converIndex = draft.conversationList.findIndex((item) => item.id === conversationId);
-				draft.conversationList[converIndex].mapping[message.id ?? nanoid()] = {
-					message: message,
-					id: message.id ?? nanoid()
-				};
+				const converID = conversationId
+					? conversationId
+					: state.conversationList[state.conversationList.length - 1]?._id;
+
+				draft.conversationList = draft.conversationList.map((item) =>
+					item.id === converID
+						? {
+								...item,
+								mapping: {
+									...item.mapping,
+									[message.id ?? nanoid()]: { message: message, id: message.id ?? nanoid() }
+								}
+						  }
+						: item
+				);
 			})
-		)
+		),
+	setCurrentConversationId: (conversationId: string | null) =>
+		set((state) => ({ currentConversationId: conversationId }))
 }));
 
 export default useConversationsStore;
